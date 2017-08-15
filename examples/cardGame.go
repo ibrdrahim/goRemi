34585d172abb6b52c14e79542a7a594c09deec70
Ibrahim <ibrdrahim@gmail.com>
2017-08-15 22:36:05 +0700

    add new function and more complex example

--- examples/cardGame.go
+++ examples/cardGame.go
@@ -1,37 +1,68 @@
 package main
-import ( 
-    "fmt"
-    "github.com/ibrdrahim/goRemi"
+
+import (
+	"fmt"
+
+	"github.com/ibrdrahim/goRemi"
 )
 
 func main() {
-    cDeck := goRemi.Init()
 
-    // initialize deck, fill deck with card
-    // // shuffle card in deck
-    cDeck.Shuffle()
-    // // show all card in deck
-    cDeck.Show()
+	// initialize Deck
+	cDeck := goRemi.InitDeck()
 
-    // initialze player
+	fmt.Printf("\n\n------- Total Card In Deck %d-------\n", len(cDeck))
 
-    players := goRemi.Players{}
+	// // show all card in deck
+	cDeck.Show()
 
-    players.Register(0,"Ibrahim")
+	// initialize deck, fill deck with card
+	// // shuffle card in deck
 
-    players.Register(1,"Computer 1")
-    
-    players.Register(2,"Computer 2")
-    
-    players.Register(3,"Computer 3")
+	fmt.Printf("\n\n------- Shuffle Deck -------\n")
 
-    for _,player := range players {
-        // draw card
-        fmt.Println("\n\n-------Draw-------\n")
-        player.DrawCards(&cDeck,7)
-        player.ShowHand()
-        fmt.Println("\n\n-------DECK-------\n")
-        cDeck.Show()
-    }
+	cDeck.Shuffle()
+	// // show all card in deck
+	cDeck.Show()
 
+	// initialze player
+
+	players := goRemi.Players{}
+
+	players.Register(0, "Ibrahim")
+
+	players.Register(1, "Computer 1")
+
+	players.Register(2, "Computer 2")
+
+	players.Register(3, "Computer 3")
+
+	// draw card from deck
+	for index := range players {
+		// draw card
+		fmt.Println("\n\n-------Draw-------")
+		players[index].DrawCards(&cDeck, 7)
+		players[index].ShowHand()
+		fmt.Println("\n\n-------DECK-------")
+		cDeck.Show()
+	}
+
+	// return card to deck
+	for index := range players {
+
+		fmt.Printf("\n\n-------%s Return All Card-------\n", players[index].Name)
+		players[index].ShowHand()
+
+		for cardIdx := 0; cardIdx < 7; cardIdx++ {
+			players[index].ThrowCards(&cDeck)
+		}
+
+		// draw card
+		fmt.Printf("\n\n-------%s Hand-------\n", players[index].Name)
+		players[index].ShowHand()
+		fmt.Println("\n\n-------DECK-------")
+		cDeck.Show()
+	}
+
+	fmt.Printf("\n\n------- Total Card In Deck %d-------\n", len(cDeck))
 }
