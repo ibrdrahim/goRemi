49c6d797c798bb40c6e9f18c82ded336fccbad0f
Ibrahim <ibrdrahim@gmail.com>
2017-08-17 20:23:46 +0700

    fix comment

--- cangkulan.go
+++ cangkulan.go
@@ -7,15 +7,18 @@ import (
 )
 
 // Cangkulan : Cangkulan The Game
+// Cangkulan Game Rules
 // 1. shuffle deck
 // 2. put 1 card from top of deck to table
 // 3. give each player 7 cards
 // 4. start playing from player 1 (clockwise)
-// 5. each player put one card with the same flag as the card on the table
+// 5. each player pick one card with the same flag as the card on top of the table, place player card on top of the table
 // 6. if player doesnt have card with the same flag draw card from deck until player get the card with the same flag
-//  6.a. if deck doesnt have any card, draw card from the top of the table
-// 7. after all player throw card to the table, compare players card, player with higher number win the round and get the score
-// 8. repeat 5-8 until deck is empty
+//  6.a. if deck doesnt have any more card, draw card from the top of the table
+// 7. after all player throw card to the table, compare players card, player with highest number win the round
+// 9. continue to play from player who win from last round, player who win last round can choose any card to play first
+// 10. repeat 5-9 until one of players doesnt have any more card
+
 type Cangkulan struct {
 	Deck    Deck
 	Field   Deck
